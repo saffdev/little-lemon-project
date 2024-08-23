9199927c53dc9829459074fa5ab4b9cc8ea4e860
SAFF19 <proyectossaff1907@gmail.com>
2024-08-23 14:32:14 -0500

    componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <main>
+        Main
+      </main>
+      <Footer />
+
+
+    </>
   );
 }
 
